2adc6159c9f506119d2890185e39c113c020e275
zshdev <shohruz.zubaidov@gmail.com>
2024-03-14 07:04:43 -0400

    Lesson 25

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from employees.models import Employee
 
 def home(request):
-    return render(request, 'home.html')
+    employees = Employee.objects.all()
+    context={
+        'employees':employees
+    }
+    return render(request, 'home.html', context)
