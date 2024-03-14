2fbb41109336c0002f35247baf8b71f99a454eb2
zshdev <shohruz.zubaidov@gmail.com>
2024-03-14 05:08:01 -0400

    admin.py Lesson 22

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='images')
+    designation = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=12, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.first_name
