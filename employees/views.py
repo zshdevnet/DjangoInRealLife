96a80756bb2d825fb8eeb545ea94f58cfc752ab9
zshdev <shohruz.zubaidov@gmail.com>
2024-03-15 00:54:37 -0400

    false commit

--- employees/views.py
+++ employees/views.py
@@ -1,1 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . models import Employee
+from django.http import HttpResponse
+
+def EmployeeDetail(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context={
+        'employee':employee,
+    }
+    return render(request, 'EmployeeDetail.html', context)
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('employees/', include('employees.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
